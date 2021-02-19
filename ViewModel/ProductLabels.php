4e3364da6427353cdeca6e55eb0074df2709e374
gaiterjones <paj@gaiterjones.com>
2021-02-19 09:20:44 +0000

    fix folder name typo

---
+++ ViewModel/ProductLabels.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Gaiterjones\ProductLabels\viewModel;
+
+
+class ProductLabels implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+
+}
